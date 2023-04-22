5543fa4e9e9d057259c280320372c477b77b138a
Arsal Azeem <azeemarsal@gmail.com>
2023-04-23 01:18:36 +0500

    configured stack navigator i.e routing

--- App.jsx
+++ App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
 
-const App = () => {
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
   return (
-    <View>
-      <Text>Hello, world!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-export default App;
+export default AppNavigator;
