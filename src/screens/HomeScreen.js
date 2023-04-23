c7d8d005940d9029a6c43f7783c307b750543f88
Arsal Azeem <azeemarsal@gmail.com>
2023-04-23 17:59:14 +0500

    using a flatlist example

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,97 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
-};
+  const friendsList = [
+    {
+      name: "Arsal Azeem"
+    }
+    ,
+    {
+      name: "Ahsan Tariq"
+    },
+    {
+      name: 'Usama Tariq'
+    }
+  ]
+  return (
+    <View>
+      <Text style={styles.plainTextStyle}>Let's begin with donow</Text>
+      <FlatList
+        data={friendsList}
+        renderItem={
+          ({item}) => {
+            return <Text style={styles.plainTextStyle}>{item.name}</Text>
+          }
+          
+        } 
+        keyExtractor={(friendsData)=>{
+          friendsData.name;
+        }}/>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-  text: {
+  plainTextStyle: {
+    color: 'black',
     fontSize: 30
   }
-});
-
+}
+);
 export default HomeScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const HomeScreen = () => {
+//   return <Text style={styles.text}>Hi there!</Text>;
+// };
+
+// const styles = StyleSheet.create({
+//   text: {
+//     color:#000,
+//     fontSize: 30
+//   }
+// });
+
+// export default HomeScreen;
