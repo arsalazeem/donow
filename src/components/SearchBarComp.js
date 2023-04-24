95bfed284790f4fd1f86d5cc394f3d1a2c6bdb6a
Arsal Azeem <azeemarsal@gmail.com>
2023-04-25 01:50:42 +0500

    created and imported yelp instance

--- src/components/SearchBarComp.js
+++ src/components/SearchBarComp.js
@@ -1,48 +1,55 @@
 import React from 'react';
-import { Text, StyleSheet, View, FlatList, Button,Image } from 'react-native';
+import { Text, StyleSheet, View, FlatList, Button, Image } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-const SearchBarComp= (props) => {
-  return (
-    <View style={styles.searchBar}>
-       <Image source={require('../assets/search.png')} style={styles.searchIcon} />
-      <TextInput style={styles.inputField}
-      placeholder='Search'
-      autoCapitalize='None'
-       />
-    </View>
+const SearchBarComp = ({ term, onChange,onSubmit }) => {
+    return (
+        <View style={styles.searchBar}>
+            <Image source={require('../assets/search.png')} style={styles.searchIcon} />
+            <TextInput
+                style={styles.inputField}
+                placeholder='Search'
+                placeholderTextColor='black'
+                selectionColor={'black'}
+                value={term}
+                onEndEditing={onSubmit}
+                onChangeText={onChange}
+                autoCapitalize='none'
+                autoCorrect={false}
+            />
+        </View>
 
-  );
+    );
 
 };
 
 const styles = StyleSheet.create({
-  searchBar: {
-    flexDirection:'row',
-    justifyContent:'flex-start',
-    color:'black',
-    borderRadius:5,
-    marginHorizontal:15,
-    marginTop:5,
-    height:50,
-    backgroundColor:'grey',
-   
-  }
-  ,
-  searchIcon:{
-    alignSelf:'center',
-    marginHorizontal:15,
-    width:35,
-    height:35
-  }
-  ,
-  inputField:{
-    paddingLeft:10,
-  flex:1,
-    // borderWidth:2,
-    // borderColor:'black',
-    color:'black',
-    fontSize:18
-  }
+    searchBar: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        color: 'black',
+        borderRadius: 5,
+        marginHorizontal: 15,
+        marginTop: 5,
+        height: 50,
+        backgroundColor: 'grey',
+
+    }
+    ,
+    searchIcon: {
+        alignSelf: 'center',
+        marginHorizontal: 15,
+        width: 35,
+        height: 35
+    }
+    ,
+    inputField: {
+        paddingLeft: 10,
+        flex: 1,
+        // borderWidth:2,
+        // borderColor:'black',
+        color: 'black',
+        fontSize: 18
+    }
 });
 
 export default SearchBarComp;
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import { Text, StyleSheet, View, FlatList, Button } from 'react-native';
 import SearchBarComp from '../components/SearchBarComp';
-const Search= (props) => {
-  return (
-    <View>
-    <SearchBarComp />
-    </View>
+import yelp from '../api/yelp';
+const Search = (props) => {
+    const [value, setValue] = useState('something');
 
-  );
+    const handleValue = (inputValue) => {
+        setValue(inputValue);
+    }
+    const handleOnSubmit=()=>{
+        alert('On Submit has been called upon');
+    }
+    return (
+        <View>
+            <SearchBarComp
+                term={value}
+                onChange={(newValue) => {
+                    handleValue(newValue);
+                }} 
+                onSubmit={()=>{
+                    handleOnSubmit();
+                }}
+                />
+            <Text style={styles.text}>{value}</Text>
+        </View>
+
+    );
 
 };
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'black',
-    fontSize: 30
-  }
+    text: {
+        color: 'black',
+        fontSize: 30
+    }
 });
 
 export default Search;
