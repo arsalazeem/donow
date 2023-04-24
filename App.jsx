e371f82c77b609775d8b2ca401739a907acd3c39
Arsal Azeem <azeemarsal@gmail.com>
2023-04-24 23:29:54 +0500

    remove other practice files

--- App.jsx
+++ App.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
-import ComponentsScreen from './src/screens/ComponentsScreen';
-import ImageDetails from './src/screens/ImageDetails';
-import ColorsDemoScreen from './src/screens/ColorsDemo';
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
@@ -12,9 +9,6 @@ const AppNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="ListDemo" component={ComponentsScreen} />
-        <Stack.Screen name="ImageDetail" component={ImageDetails} />
-        <Stack.Screen name="ColorsDemo" component={ColorsDemoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,39 +3,9 @@ import { Text, StyleSheet, View, FlatList, Button } from 'react-native';
 const HomeScreen = (props) => {
   console.log(props.navigation.navigate);
   return (
-    <View>
-      <Text style={styles.text}>Hi there!</Text>
-      <Button 
-      onPress={()=>{
-         props.navigation.navigate('ListDemo');
-      }}
-      title='Go to list demo screen'
-      />
-         <Button 
-      onPress={()=>{
-         props.navigation.navigate('ImageDetail');
-      }}
-      title='Go to Image Detail Screen'
-      />
-      <Button 
-      onPress={()=>{
-         props.navigation.navigate('ColorsDemo');
-      }}
-      title='Go to Colors Demo Screen'
-      />
-        <Button 
-      onPress={()=>{
-         props.navigation.navigate('CountersDemo');
-      }}
-      title='Go to Colors Counter Demo Screen'
-      />
-      <Button 
-      onPress={()=>{
-         props.navigation.navigate('TextsDemo');
-      }}
-      title='Go to Colors Text Demo Screen'
-      />
-    </View>
+ <View>
+  <Text>This is a fresh view</Text>
+ </View>
 
   );
 
