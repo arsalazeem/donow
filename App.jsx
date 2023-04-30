b126ada87e6b5b90010d48a752dd54988ff5578b
arsalazeem24 <azeemarsal@gmail.com>
2023-05-01 00:01:00 +0500

    blogapp

--- App.jsx
+++ App.jsx
@@ -1,69 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import GpaCalculator from './src/screens/GpaScreens/GpaCalculator';
-import CgpaCalculator from './src/screens/CGPAScreens/CgpaCalculator';
-import GpaCalculationScreen from './src/screens/GpaScreens/GpaCalculationScreen';
-import CGpaCalculationScreen from './src/screens/CGPAScreens/CGpaCalculationScreen';
+import IndexScreen from './src/screens/IndexScreen';
 const Stack = createStackNavigator();
 const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{
-        title: "Select Type",
-        headerTitleAlign: "center",
-        headerTintColor: "white",
-        headerStyle:{
-          backgroundColor:"#343a40"
-        }
-      }}
-    />
+   
+
      <Stack.Screen
-      name="Gpa"
-      component={GpaCalculator}
+      name="IndexScreen"
+      component={IndexScreen}
       options={{
-        title: "GPA Calculator",
-        headerTitleAlign: "center",
-        headerTintColor: "white",
-        headerStyle:{
-          backgroundColor:"#343a40"
-        }
-      }}
-    />
-      <Stack.Screen
-      name="Cgpa"
-      component={CgpaCalculator}
-      options={{
-        title: "CGPA Calculator",
-        headerTitleAlign: "center",
-        headerTintColor: "white",
-        headerStyle:{
-          backgroundColor:"#343a40"
-        }
-      }}
-    />
-     <Stack.Screen
-      name="CgpaCal"
-      component={CGpaCalculationScreen}
-      options={{
-        title: "CGPA Calculation",
-        headerTitleAlign: "center",
-        headerTintColor: "white",
-        headerStyle:{
-          backgroundColor:"#343a40"
-        }
-      }}
-    />
-     <Stack.Screen
-      name="GpaCal"
-      component={GpaCalculationScreen}
-      options={{
-        title: "GPA Calculation",
+        title: "Index Screen",
         headerTitleAlign: "center",
         headerTintColor: "white",
         headerStyle:{
@@ -75,5 +25,7 @@ const AppNavigator = () => {
     </NavigationContainer>
   );
 };
-
-export default AppNavigator;
+const App=AppNavigator;
+export default ()=>{
+return <App />;
+}
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet, View, FlatList, Button } from 'react-native';
+const IndexScreen = (props) => {
+  return (
+    <View style={styles.container}>
+         <Text style={{color:"black"}}>I am on the IndexScreen </Text>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default IndexScreen;
