8067309c2b5d8f4b24f73cac8386f42c649827b9
arsalazeem24 <azeemarsal@gmail.com>
2023-05-06 16:12:03 +0500

    created basic provider

--- App.jsx
+++ App.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import IndexScreen from './src/screens/IndexScreen';
+import {BlogProvider} from './src/context/BlogContext';
 const Stack = createStackNavigator();
 const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-   
-
-     <Stack.Screen
-      name="IndexScreen"
-      component={IndexScreen}
-      options={{
-        title: "Index Screen",
-        headerTitleAlign: "center",
-        headerTintColor: "white",
-        headerStyle:{
-          backgroundColor:"#343a40"
-        }
-      }}
-    />
+        <Stack.Screen
+          name="IndexScreen"
+          component={IndexScreen}
+          options={{
+            title: 'Index Screen',
+            headerTitleAlign: 'center',
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: '#343a40',
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-const App=AppNavigator;
-export default ()=>{
-return <App />;
-}
+const App = AppNavigator;
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
